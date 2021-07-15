40fe4934a81b561654f857ab421a8c95e597c553
selmantercioglu <selmantercioglu@gmail.com>
2021-07-15 12:19:45 +0300

    add library

---
+++ library/specialFunction.py
@@ -0,0 +1,34 @@
+import os
+import glob
+
+
+class sf():
+    
+    @staticmethod
+    def getOsType():
+        if (os.name == 'posix'):
+            return "linux"
+        elif (os.name == "nt"): 
+            return "windows"
+        else:
+            return "[WARNING!]: isletim sistemi tespitinde hata! Lutfen root/lib/specialFunctions.py/sf/getOsType metodunu Kontrol edin! "
+
+    @staticmethod
+    def clear():
+        if(sf.getOsType()  == "linux"):
+            os.system("clear")
+        elif (sf.getOsType == "windows"):
+            os.system("cls")
+    
+    
+    @staticmethod
+    def getFileList(path):
+        return glob.glob(path)
+
+    @staticmethod 
+    def getFileListWithType(path, type):        
+        data = glob.glob(path + type)
+        return data
+
+a = sf()
+x = a.getFileListWithType("", "*.py")
